edaacc4be1ddf8fc9ac36e8f78dfcca43f061a5b
Kyle Nguyen <kyleqbnguyen@gmail.com>
2026-03-02 11:31:19 -0800

    refactor: rename file and move struct into namespace

--- src/core/render_config.h
+++ src/core/render_config.h
@@ -5,9 +5,13 @@
 
 enum class FileCategory : std::int8_t { Audio, Video, Image };
 
+namespace RenderConfig {
+
 struct Config {
   std::filesystem::path audioPath;
   std::filesystem::path visualPath;
   std::filesystem::path outputPath;
   FileCategory visualType;
 };
+
+} // namespace RenderConfig 
