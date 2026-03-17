d784deaa448c2ad2750afc3707004f7c50d1e00f
Kyle Nguyen <kyleqbnguyen@gmail.com>
2026-03-17 13:32:02 -0700

    fix: icon not appearing in windows taskbar

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "ui/main_window.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   app.setApplicationName(QStringLiteral("TypeBeat"));
   app.setOrganizationName(QStringLiteral("TypeBeat"));
+  app.setWindowIcon(QIcon(QStringLiteral(":/TypeBeat.png")));
 
   ui::MainWindow window;
   window.show();
